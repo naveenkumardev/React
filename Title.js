8b41cdfbcdd312fd815da363c6fd5a234bb0c798
naveenkumardev <85108878+naveenkumardev@users.noreply.github.com>
2022-09-30 09:53:39 +0530

    Add files via upload

---
+++ Title.js
@@ -0,0 +1,12 @@
+
+import React , {Component } from 'react'
+
+class Title extends Component {
+    render()
+            {
+              return ( <h1> {this.props.headerTitle} </h1>)
+  
+  }
+}
+
+export default Title;
